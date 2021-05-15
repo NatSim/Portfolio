a4c3e8f3e2fa4ba62387f5d6f5f7edb74cfc75f0
NatSim <natsimbig@gmail.com>
2021-05-15 15:47:34 +0100

    adding backend contact form

---
+++ NatSim(Website)/server/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const path = require("path");
+
+require("dotenv").config();
+console.log(process.env.PASSWORD);
+
+//Middleware
+app.use(express.static("../client/public"));
+app.use(express.json());
+
+//REQUEST
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/public", "index.html"));
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: { user: "natsimbig@gmail.com", pass: "12345" },
+  });
+
+  const mailOptions = {
+    from: req.body.email,
+    to: "natsimbig@gmail.com",
+    subject: `Message from ${req.body.email}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("Error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Success!!");
+    }
+  });
+});
+
+//Server PORT
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
